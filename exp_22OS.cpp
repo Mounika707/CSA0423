186c19ce99db8a6970b12a4f5be4d89e8ee76eda
Mounika M <mounikamuraboina707@gmail.com>
2025-12-04 15:59:36 +0530

    Add files via upload

---
+++ exp_22OS.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void bestFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n]; // stores block assigned to each process
+
+    // initially no process is allocated
+    for (int i = 0; i < n; i++) {
+        allocation[i] = -1;
+    }
+
+    // pick each process and find suitable block
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx]) {
+                    bestIdx = j;
+                }
+            }
+        }
+
+        // if found
+        if (bestIdx != -1) {
+            allocation[i] = bestIdx;
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+
+    printf("\nProcess No.\tProcess Size\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        printf(" %d\t\t%d\t\t", i + 1, processSize[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+}
+
+int main() {
+    int m, n;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &m);
+    int blockSize[m];
+    printf("Enter sizes of memory blocks:\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &blockSize[i]);
+    }
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    int processSize[n];
+    printf("Enter sizes of processes:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &processSize[i]);
+    }
+
+    bestFit(blockSize, m, processSize, n);
+
+    return 0;
+}
